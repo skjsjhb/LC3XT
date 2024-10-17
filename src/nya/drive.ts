4f1dc94c2f65bdb09f818deefe2ed44d18397063
skjsjhb <skjsjhb@outlook.com>
2024-10-17 11:49:35 +0800

    Add test driver for lab 2

--- src/nya/drive.ts
+++ src/nya/drive.ts
@@ -2,6 +2,7 @@ import type { HaltReason, VM } from "../sugar/vm";
 import type { TestUnitResult, TestUnitStatus } from "./context";
 import hello from "./drivers/hello";
 import lab1 from "./drivers/lab1";
+import lab2 from "./drivers/lab2";
 
 export type TestExecutor = (
     vm: VM,
@@ -22,6 +23,11 @@ const drivers: Record<string, TestDriver> = {
     lab1: {
         repeat: 10,
         exec: lab1,
+    },
+
+    lab2: {
+        repeat: 20,
+        exec: lab2,
     },
 };
 
